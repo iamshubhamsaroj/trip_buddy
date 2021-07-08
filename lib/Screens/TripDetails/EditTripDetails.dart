0bf2626f73bdd61431060b2acc9bd76dac6584a4
saurabh-pandeyy <saurabhp.dev@gmail.com>
2021-07-08 18:15:41 +0530

    updated delete feature

--- lib/Screens/TripDetails/EditTripDetails.dart
+++ lib/Screens/TripDetails/EditTripDetails.dart
@@ -17,7 +17,6 @@ class EditTripDetails extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
 
-
       appBar: AppBar(
         title: Text('Edit Trip Details'),
       ), 
